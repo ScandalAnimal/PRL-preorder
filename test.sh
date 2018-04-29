48ab6f6c074f55dc19004f5d94577534a44d9cee
Maros Vasilisin <xvasil02@vutbr.cz>
2018-04-29 17:45:37 +0200

    archive, doc, renamed

--- test.sh
+++ test.sh
@@ -9,8 +9,8 @@ list=$1
 size=${#list}
 let proc_no="(2*$size)-2"
 
-mpic++ --prefix /usr/local/share/OpenMPI -o preorder preorder.cpp
+mpic++ --prefix /usr/local/share/OpenMPI -o pro pro.cpp
 
-mpirun --prefix /usr/local/share/OpenMPI -np $proc_no preorder $1
+mpirun --prefix /usr/local/share/OpenMPI -np $proc_no pro $1
 
-rm -f preorder
+rm -f pro
